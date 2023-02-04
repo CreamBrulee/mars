d9f6bfa581fcafdd3e31a2d1a5c9e44fee27cf10
CreamBrulee <107671679+CreamBrulee@users.noreply.github.com>
2023-02-04 14:32:10 +0300

    Add files via upload

---
+++ Готовимся к миссии/Ihateyandexlyceum.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index1():
+    return render_template('base.html', title='MeowMeow')
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title=prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
